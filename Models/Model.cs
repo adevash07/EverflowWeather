9b22d41f86f63c8a9602ab733c263c50d38274fd
debugger <58630922+adevash07@users.noreply.github.com>
2023-05-18 13:39:17 +0100

    Fix: Solve Unload error, Solve location bug, implement Dan Suggestioin

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,19 +1,6 @@
  using Newtonsoft.Json;
 
 namespace EverFlowWeather.Models;
-// {
-//     public class Weather
-//     {
-//         [JsonProperty]
-//         public double Temperature { get; set; }
-//         [JsonProperty]
-//         public string WeatherDescription { get; set; } = " ";
-//         [JsonProperty]
-//         public string IconUrl { get; set; } = " ";
-//         [JsonProperty]
-//         public DateTime Date { get; set; }
-//     }
-// }
 
 public class Main
 {
@@ -89,3 +76,11 @@ public class root
     public int cnt { get; set; }
     public List<Forecast>? list { get; set; }
 }
+
+  // Define a class that represents the geo response object
+    public class GeoResponse
+    {
+        public string? name { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+    }
